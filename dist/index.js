2ff430bffe57824a2989479cc8124541529c610e
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2022-10-28 18:46:26 -0300

    Alterando .gitignore

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
-app.get('/', (request, response) => {
-    return response.json({
-        'msg': 'Hello World!'
-    });
-});
+app.get('/', routes_1.default);
 app.listen(3333);
